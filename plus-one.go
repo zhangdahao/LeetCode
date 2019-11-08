43de6670d5eee01a5a000ffaab11d1b9a0392a46
super_zdh <31751977+zhangdahao@users.noreply.github.com>
2019-11-08 15:55:54 +0800

    Create plus-one.go
    加一

---
+++ plus-one.go
@@ -0,0 +1,19 @@
+//加一
+func plusOne(digits []int) []int {
+	flag := 1
+	for i := len(digits) - 1; i >= 0; i-- {
+		if flag != 0 {
+			if digits[i]+1 > 9 {
+				flag = 1
+				digits[i] = 0
+			} else {
+				flag = 0
+				digits[i]++
+			}
+			if flag != 0 && i == 0 {
+				digits = append([]int{1}, digits...)
+			}
+		}
+	}
+	return digits
+}
